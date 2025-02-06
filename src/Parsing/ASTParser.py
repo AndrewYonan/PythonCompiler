2a2031b48406e986778a8a17d271ea6b01ba042b
= <andrew.yonan01@gmail.com>
2025-02-05 17:50:04 -0700

    beginnings of a parser - single expressions, prints, and parentheses working

--- src/Parsing/ASTParser.py
+++ src/Parsing/ASTParser.py
@@ -2,6 +2,8 @@ import re
 import ast
 from ast import *
 from ASTClasses import *
+from ASTDump import *
+
 
 TOKEN_INT = "INT"
 TOKEN_PLUS = "PLUS"
@@ -12,19 +14,21 @@ TOKEN_LPAREN = "LPAREN"
 TOKEN_RPAREN = "RPAREN"
 TOKEN_PRINT = "PRINT"
 TOKEN_EVAL_INPUT = "EVAL_INPUT"
+TOKEN_NEWLINE =  "NEWLINE"
 TOKEN_EOF = "EOF"
 
 
 token_spec = [(r'print', TOKEN_PRINT),
               (r'eval\(\s*input\(\)\s*\)', TOKEN_EVAL_INPUT),
+              (r'[a-zA-Z_][a-zA-Z0-9_]*', TOKEN_VAR),
               (r'\d+', TOKEN_INT),
               (r'\+', TOKEN_PLUS),
               (r'\-', TOKEN_MINUS),
-              (r'[a-zA-Z_][a-zA-Z0-9_]*', TOKEN_VAR),
               (r'=', TOKEN_EQUAL),
               (r'\(', TOKEN_LPAREN),
               (r'\)', TOKEN_RPAREN),
-              (r'\s+', None)]
+              (r'\n', TOKEN_NEWLINE),
+              (r'\s+', None)] # to ignore whitespace
 
 
 
@@ -72,21 +76,100 @@ class Parser:
 
     def __init__(self, lexer):
         self.lexer = lexer
-    
+        self.current_token = self.lexer.get_next_token()
+        self.module_body = []
 
+    def at_prog_end(self):
+        return self.current_token[0] == TOKEN_EOF
+    
+    def consume(self, token_type):
+        if self.current_token[0] == token_type:
+            self.current_token = self.lexer.get_next_token()
+            # print(f"[+] parser consumed token {token_type}")
+        else:
+            print(f"ERR : unexpected token {self.current_token}")
+            exit(1)
+    
+    def strip_newlines(self):
+        while self.current_token[0] == TOKEN_NEWLINE:
+            self.consume(TOKEN_NEWLINE)
+    
+    def factor(self):
+        token = self.current_token
+        if token[0] == TOKEN_INT:
+            self.consume(TOKEN_INT)
+            return Constant(value=int(token[1]))
+        if token[0] == TOKEN_LPAREN:
+            self.consume(TOKEN_LPAREN)
+            node = self.expr()
+            self.consume(TOKEN_RPAREN)
+            return node
+    
+    def term(self):
+        return self.factor()
+    
+    def expr(self):
+        node = self.term()
+        while self.current_token[0] == TOKEN_PLUS:
+            token = self.current_token
+            self.consume(TOKEN_PLUS)
+            node = BinOp(left = node,
+                        op = Add(),
+                        right = self.term())
+        return node
+    
+    def simple_statement(self):
+        if self.current_token[0] == TOKEN_PRINT:
+            self.consume(TOKEN_PRINT)
+            self.consume(TOKEN_LPAREN)
+            node = Call(func=Name(id="print", ctx=Load()),
+                        args=[self.expr()],
+                        keywords=[])
+            self.consume(TOKEN_RPAREN)
+            return Expr(value=node)
+        
+        else:
+            return Expr(value = self.expr())
+    
+    def get_next_statement(self):
+        self.strip_newlines()
+        if self.at_prog_end():
+            return None
+        return self.simple_statement()
+
+    def parse(self):
+
+        self.strip_newlines()
+
+        while not self.at_prog_end():
+            statement = self.get_next_statement()
+            if statement:
+                self.module_body.append(statement)
+
+        return Module(
+            body = self.module_body,
+            type_ignores=[]
+        )
 
 
 
 if __name__ == "__main__":
 
-    prog = """x=1
-    y=2
-    print(1-(x + y + eval(input)))"""
+    prog = """(((1)))"""
 
     lex = Lexer(prog)
     lex.tokenize()
 
-    while lex.empty() == False:
-        print(lex.get_next_token())
-    
-    pass
+    print("=====prog TOKENS=====")
+
+    print(f"{lex.tokens}")
+
+    print(f"from astParse:\n {ast.dump(ast.parse(prog), indent=3)}")
+
+    print("=====Parse results======")
+
+    parser = Parser(lex)
+    tree = parser.parse()
+
+    dumper = ASTDump()
+    print(dumper.dump(tree))
